7a66c661e2c8b89c9d5441d074aff1a731e26740
Rach98-del <rachanadmahajan@gmail.com>
2025-03-24 22:40:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import snowflake.connector as sc
+#import snowflake.connector as sc
 
 helpful_links = [
     "https://docs.streamlit.io",
